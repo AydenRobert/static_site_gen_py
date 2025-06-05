6b70c7f39cc4c9044f4dcdb7b2e8c3124a3b6296
AydenRobert <aydenroberthollingsworth@gmail.com>
2025-06-05 23:28:00 +1000

    checkpoint before rm

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
-from mdtohtml import markdown_to_html_node
+import os
+import shutil
 
 
 def main():
-
+    current_dir = os.getcwd()
+    if current_dir.split("/")[-1] != "static_site_gen_py":
+        raise Exception("in wrong dir aye")
+    os.rmdir("public")
+    shutil.copytree("static", "public", dirs_exist_ok=True)
 
 if __name__ == "__main__":
     main()
