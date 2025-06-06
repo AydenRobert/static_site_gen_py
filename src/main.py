666c9f80eccee9b131a154fcb582125479f1a0f8
AydenRobert <aydenroberthollingsworth@gmail.com>
2025-06-06 16:11:37 +1000

    second test

--- src/main.py
+++ src/main.py
@@ -4,23 +4,25 @@ import shutil
 import sys
 
 
-def setup_dirs():
+def setup_dirs(public_dir):
     current_dir = os.getcwd()
     if current_dir.split("/")[-1] != "static_site_gen_py":
         raise Exception("in wrong dir aye")
-    shutil.rmtree("public")
-    shutil.copytree("static", "public", dirs_exist_ok=True)
+    shutil.rmtree(public_dir, ignore_errors=True)
+    shutil.copytree("static", public_dir, dirs_exist_ok=True)
 
 
 def main():
-    setup_dirs()
     base_dir = ""
-    if len(sys.argv) == 2:
-        base_dir = sys.argv[1]
-
     content_dir = base_dir + "content"
     template_file = base_dir + "template.html"
-    public_dir = base_dir + "public"
+    public_dir = base_dir + "docs"
+    setup_dirs(public_dir)
+    if len(sys.argv) >= 2:
+        base_dir = sys.argv[1]
+    if len(sys.argv) >= 3:
+        public_dir = sys.argv[2]
+
     generate_pages_recursively(
         content_dir, template_file, public_dir, base_dir)
 
